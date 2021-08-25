22dc92741f1986351e996c4b2a7f7adc5376bb07
Spencer Williams <s@spencerenglish.com>
2021-08-24 19:44:16 -0700

    hmmm

--- Constants.h
+++ Constants.h
@@ -14,3 +14,5 @@
 #else
 #   define FourCC2Str(fourcc) (const char[]){*(((char*)&fourcc)+3), *(((char*)&fourcc)+2), *(((char*)&fourcc)+1), *(((char*)&fourcc)+0),0}
 #endif
+#define SCREEN_WIDTH fmax(UIScreen.mainScreen.bounds.size.width, UIScreen.mainScreen.bounds.size.height)
+#define SCREEN_HEIGHT fmin(UIScreen.mainScreen.bounds.size.width, UIScreen.mainScreen.bounds.size.height)
